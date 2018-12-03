4c023128a66459ee0dd8f3d2470dbac721a3e04a
WyattLiew <weijunn@hotmail.com>
2018-12-03 18:02:07 +0800

    v1.7

--- Web/js/login.js
+++ Web/js/login.js
@@ -34,9 +34,11 @@ function login() {
   	window.alert("Error : " + errorMessage);
     // ...
     	loginProgress.style.display="none"
-		loginButton.style.display="block"
+		  loginButton.style.display="block"
 	
 	});
+}else{
+  alert("Please fill in the blank.")
 }
 }
 
@@ -86,3 +88,56 @@ function signOutBtn(){
     }
 
 })(window);
+
+// reference ID part 1
+
+function refSubmitclick() {
+  var referenceID = document.getElementById("referenceIdText").value;
+  if (referenceID !="") {
+    queryDatabase(referenceID);
+  }else{
+    alert("Please enter your reference id");
+  }
+}
+
+function queryDatabase(referenceID){
+  firebase.database().ref('/Defect Add On/' + referenceID).once('value').then(function(snapshot){
+    var defectObject = snapshot.val();
+    console.log(defectObject);
+    var keys = Object.keys(defectObject);
+    for (var i = 0; i < keys.length; i++){
+      var currentObject = defectObject[keys[i]];
+      var element = document.getElementById("contentHolder");
+      var currentRow;
+      if(i % 3 == 0){
+          currentRow = document.createElement("div");
+          currentRow.classList.add("row");
+          element.append(currentRow);
+      }
+      var col = document.createElement("div");
+      col.classList.add("col-lg-4");
+      var image = document.createElement("img");
+      image.src = currentObject.imgURL;
+      image.classList.add("contentImage");
+      var date = document.createElement("p");
+      var node = document.createTextNode("Date: " + currentObject.date);
+      date.appendChild(node);
+      var defect = document.createElement("p");
+      var node1 = document.createTextNode("Defect: " + currentObject.defect);
+      defect.appendChild(node1);
+      var comment = document.createElement("p");
+      var node2 = document.createTextNode("Comment: " + currentObject.comments);
+      comment.appendChild(node2);
+      col.append(image);
+      col.append(date);
+      col.append(defect);
+      col.append(comment);
+      currentRow.append(col);
+      image.onclick = function(){ console.log(currentObject.id)}
+      }
+  }).catch(function(error){
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    window.alert("Error : Please check your reference ID, it's case sensitive.");
+  });
+}
